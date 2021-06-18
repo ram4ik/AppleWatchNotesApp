d05c3c0d12b17e8024521f67b791a3b73b07ce2d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-06-18 20:21:06 +0300

    Initial Commit

---
+++ AppleWatchNotesApp WatchKit Extension/AppleWatchNotesAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  AppleWatchNotesAppApp.swift
+//  AppleWatchNotesApp WatchKit Extension
+//
+//  Created by Ramill Ibragimov on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ AppleWatchNotesApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleWatchNotesApp WatchKit Extension
+//
+//  Created by Ramill Ibragimov on 18.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
